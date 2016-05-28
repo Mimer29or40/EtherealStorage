c18c2fd49cb897268611ed23995eaf3680b2c8cc
Mimer29or40 <rjs007@live.com>
2016-05-28 19:53:13 -0400

    Initial Commit

---
+++ src/main/java/mimer29or40/etherealStorage/EtherealStorage.java
@@ -0,0 +1,20 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = EtherealStorage.MODID, version = EtherealStorage.VERSION)
+public class EtherealStorage
+{
+    public static final String MODID   = "examplemod";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
+    }
+}
